81dc344d9228e96e63d9650bd31a8e7c774fcbd3
kazuki higashi <kazukihigashi@kazukinoMacBook-Air.local>
2022-04-22 22:11:39 +0900

    PC着手

--- JavaScript.js/script.js
+++ JavaScript.js/script.js
@@ -2,12 +2,16 @@
 
 $(".c-button--menu").click(function () 
 {//ボタンがクリックされたら
-  $(this).toggleClass('open');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+
+
+  $(this).toggleClass('l-sideber');
+    //ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');
+    //ナビゲーションにpanelactiveクラスを付与
 });
 
 $("#g-nav a").click(function () 
 {//ナビゲーションのリンクがクリックされたら
-    $(".c-button--menu").removeClass('open');//ボタンの activeクラスを除去し
+    $(".c-button--menu").removeClass('l-sideber');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
