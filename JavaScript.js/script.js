7cd934a896702b162c3ce32e7fccffd8bc40e24f
kazuki higashi <kazukihigashi@kazukinoMacBook-Air.local>
2022-04-27 16:49:24 +0900

    フロント修正

--- JavaScript.js/script.js
+++ JavaScript.js/script.js
@@ -1,6 +1,7 @@
 $(function() {
 
-$('.c-button--menu').on("click",function () 
+$('.p-head__menu').on("click",function () 
+
 {//ボタンがクリックされたら
 
   $('.l-sideber').toggleClass('open');
@@ -9,12 +10,14 @@ $('.c-button--menu').on("click",function ()
     // activeクラスを付与
 });
 
-// $('.c-button--menu').click(function () 
-// {//ナビゲーションのリンクがクリックされたら
-//     $('.c-button--menu').removeClass('open');
-//     //ボタンの activeクラスを除去し
-//     $('.c-cover').removeClass('active');
-//     //オーバーレイのactiveクラスも除去
-// });
+$('.c-button__close').on("click",function () 
+
+{//ナビゲーションのリンクがクリックされたら
+
+    $('.l-sideber').removeClass('open');
+    //ボタンの activeクラスを除去し
+    $('.c-cover').removeClass('active');
+    //オーバーレイのactiveクラスも除去
+});
 
 });
