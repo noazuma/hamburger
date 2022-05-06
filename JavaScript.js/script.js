0368e6eaf93a72ca3bf74bbfd27ff6b0d492fe86
kazuki higashi <kazukihigashi@kazukinoMacBook-Air.local>
2022-05-06 20:01:37 +0900

    フロント修正

--- JavaScript.js/script.js
+++ JavaScript.js/script.js
@@ -32,16 +32,48 @@ $('.c-button__close').on("click",function ()
     // 初期値に戻す
 });
 
-// $('.p-serch__box').on("click",function () 
+$(window).resize(function(){
 
-// {//ボタンがクリックされたら
-
-//   $('.c-icon').toggleClass('display',':none');
-//     //虫メガネを消す
   
-//   $('.').removeClass('open');
+    var width = $(window).width();
+   
+   
+    // 条件を設定する
+    if (width <= 1026) {
+      $(".l-sideber").css("transition", "none");
+    }
+    // else {
+    //   $("h2").css("background", "");
+    // }
+   
+  
+
+  // $('.l-sideber').removeClass('open');
+  //   //ボタンの activeクラスを除去し
+  //   $('.c-cover').removeClass('active');
+  //   //オーバーレイのactiveクラスも除去
+  //   $('.c-line__menu1').removeClass('open');
+  //   // バツ動き1　除去
+  //   $('.c-line__menu2').removeClass('open');
+  //   // バツ動き2 除去
+  //   $('body').css('overflow','visible');
+  // let width = $(window).width();
+  // let height = $(window).height();
+ 
+  // $('l-sideber').removeClass('open');
+  // $(".output2").text("縦のサイズ" + height + "px");
 
 
 
-// });
+
+  // $('body').css('overflow','visible');
+  // 条件を設定する
+  // if (width <= 1025) {
+  //   $('l-sideber').css('transtion', 'none');
+  // }
+  // else {
+  //   $('l-sideber').css('transtion', '');
+  // }
+ 
+});
 });
